f85d298e220d826b47701ab3228d73ec137e3d55
Laurent Binder <laurentbinder@gmail.com>
2021-01-06 15:25:45 +0100

    add js init_letters for logo animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,8 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+// homemade plugins
+import initLetters from "../plugins/init_letters";
+
+initLetters();
---
+++ app/javascript/plugins/init_letters.js
@@ -0,0 +1,40 @@
+const initLetters = () => {
+  var textWrapper = document.querySelector('.moving-letters .letters');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  anime.timeline({loop: false})
+    .add({
+      targets: '.moving-letters .line',
+      opacity: [0.5,1],
+      scaleX: [0, 1],
+      easing: "easeInOutExpo",
+      duration: 700
+    }).add({
+      targets: '.moving-letters .line',
+      duration: 600,
+      easing: "easeOutExpo",
+      translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+    }).add({
+      targets: '.moving-letters .logo-img',
+      opacity: [0,1],
+      scaleY: [0.5, 1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=600'
+    }).add({
+      targets: '.moving-letters .letters-left',
+      opacity: [0,1],
+      translateX: ["0.5em", 0],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=300'
+    }).add({
+      targets: '.moving-letters .letters-right',
+      opacity: [0,1],
+      translateX: ["-0.5em", 0],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=600'
+    });
+}
+
+export default initLetters;
